c6811f7f1fc81972198dc6c5dbf2c8697134edd2
Millyarde <admin@millyarde.com>
2024-01-03 01:06:09 -0600

    add mask nodes

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 import os
-from .pomfy import FromPhotoshop, ToPhotoshop
+from .pomfy import FromPhotoshop, FromPhotoshopMask, ToPhotoshop, ToPhotoshopMask
 
 POMFY_VERSION = "0.1.0"
 
 NODE_CLASS_MAPPINGS = {}  # Define the dictionary here
 
 NODE_CLASS_MAPPINGS["To Photoshop"] = ToPhotoshop
+NODE_CLASS_MAPPINGS["To Photoshop Mask"] = ToPhotoshopMask
 NODE_CLASS_MAPPINGS["From Photoshop"] = FromPhotoshop
+NODE_CLASS_MAPPINGS["From Photoshop Mask"] = FromPhotoshopMask
 
 WEB_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), "js")
 __all__ = ['NODE_CLASS_MAPPINGS', "WEB_DIRECTORY"]

8f35492162d74713d9d979ac3776442561190d32
Millyarde <admin@millyarde.com>
2024-01-13 01:51:45 -0600

    mock websocket send/recieve

--- pomfy.py
+++ pomfy.py
@@ -1,14 +1,8 @@
 from server import PromptServer
 import torch
-
-def message(id, message):
-    if isinstance(message, torch.Tensor):
-        string = f"Tensor shape {message.shape}"
-    elif isinstance(message, dict) and "samples" in message and isinstance(message["samples"], torch.Tensor):
-        string = f"Latent shape {message['samples'].shape}"
-    else:
-        string = f"{message}"
-    PromptServer.instance.send_sync("pomfy-message-handler", {"id": id, "message": string})
+import base64
+import json
+import asyncio
 
 class BasePhotoshopOperation():
     CATEGORY = "photoshop"
@@ -16,68 +10,80 @@ class BasePhotoshopOperation():
     INPUT_TYPES = {}
     RETURN_TYPES = {}
 
-class FromPhotoshop(BasePhotoshopOperation):
+class BaseWebSocketListener:
+    async def websocket_listener(self, ws, layerName, dataType):
+        try:
+            async for msg in ws:
+                if msg.type == aiohttp.WSMsgType.TEXT:
+                    data = json.loads(msg.data)
+                    if dataType in data and "layerName" in data and data["layerName"] == layerName:
+                        decoded_data = data[dataType]
+                        return base64.b64decode(decoded_data)
+        except Exception as e:
+            print(f'WebSocket listener error: {e}')
+        return None
+
+class FromPhotoshop(BasePhotoshopOperation, BaseWebSocketListener):
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {"layerName": ("STRING", {"default": ""})},
             "optional": {}
         }
-    
+
     RETURN_NAMES = ("image",)
     RETURN_TYPES = ("IMAGE",)
 
-    def func(self, id, **kwargs):
-        for key in kwargs:
-            message(id, kwargs[key])
-        return ()
+    def func(self, layerName):
+        image_data = asyncio.run(self.websocket_listener(
+            PromptServer.instance.sockets[self.client_id], layerName, "image"
+        ))
+        return (image_data,)
 
-class FromPhotoshopMask(BasePhotoshopOperation):
+class FromPhotoshopMask(BasePhotoshopOperation, BaseWebSocketListener):
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {"layerName": ("STRING", {"default": ""})},
             "optional": {}
         }
-    
+
     RETURN_NAMES = ("mask",)
     RETURN_TYPES = ("MASK",)
 
-    def func(self, id, **kwargs):
-        for key in kwargs:
-            message(id, kwargs[key])
-        return ()
+    def func(self, layerName):
+        mask_data = asyncio.run(self.websocket_listener(
+            PromptServer.instance.sockets[self.client_id], layerName, "mask"
+        ))
+        return  (mask_data,)
+
 
 class ToPhotoshop(BasePhotoshopOperation):
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {"image": ("IMAGE",)},
-            "optional": {
-                "layerName": ("STRING", {"default": ""})
-            }
+            "optional": {"layerName": ("STRING", {"default": ""})}
         }
 
-    OUTPUT_NODE = true
+    OUTPUT_NODE = True
 
-    def func(self, id, **kwargs):
-        for key in kwargs:
-            message(id, kwargs[key])
+    def func(self, image, layerName):
+        image_data = ("PNG", image)
+        PromptServer.instance.send(BinaryEventTypes.UNENCODED_PREVIEW_IMAGE, image_data)
         return ()
-        
+
 class ToPhotoshopMask(BasePhotoshopOperation):
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {"mask": ("MASK",)},
-            "optional": {
-                "layerName": ("STRING", {"default": ""}),
-            }
+            "optional": {"layerName": ("STRING", {"default": ""})}
         }
 
-    OUTPUT_NODE = true
+    OUTPUT_NODE = True
 
-    def func(self, id, **kwargs):
-        for key in kwargs:
-            message(id, kwargs[key])
+    def func(self, mask, layerName):
+        mask_data = ("PNG", mask)
+        PromptServer.instance.send_sync("image", mask_data)
         return ()
